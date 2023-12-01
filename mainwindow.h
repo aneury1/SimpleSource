59aa4f02bb82fefb2b6ff6caf32da96798985cd8
aneury1 <aneury1@gmail.com>
2023-12-01 12:50:08 -0400

    Starting point

--- mainwindow.h
+++ mainwindow.h
@@ -15,90 +15,52 @@
 
 #include <QMainWindow>
 
-#include "codeeditor.h"
-
 QT_BEGIN_NAMESPACE
 class QAction;
-class QActionGroup;
-class QLabel;
 class QMenu;
+class QTextEdit;
 QT_END_NAMESPACE
 
-//! [0]
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+
+class MainWindow : public QMainWindow{
+
+    Q_OBJECT;
+
+    public:
+
+        MainWindow();
 
 
-    CodeEditor *editor;
+    private slots:
 
-public:
-    MainWindow();
+        void about();
 
-protected:
-#ifndef QT_NO_CONTEXTMENU
-    void contextMenuEvent(QContextMenuEvent *event) override;
-#endif // QT_NO_CONTEXTMENU
-    //! [0]
+    private:
 
-    //! [1]
-private slots:
-    void newFile();
-    void open();
-    void save();
-    void print();
-    void undo();
-    void redo();
-    void cut();
-    void copy();
-    void paste();
-    void bold();
-    void italic();
-    void leftAlign();
-    void rightAlign();
-    void justify();
-    void center();
-    void setLineSpacing();
-    void setParagraphSpacing();
-    void about();
-    void aboutQt();
-    //! [1]
+        void createStatusBar();
+        void createDockWindow();
+        void createActions();
 
-    //! [2]
-private:
-    void createActions();
-    void createMenus();
-    //! [2]
 
-    //! [3]
-    QMenu *fileMenu;
-    QMenu *editMenu;
-    QMenu *formatMenu;
-    QMenu *helpMenu;
-    QActionGroup *alignmentGroup;
-    QAction *newAct;
-    QAction *openAct;
-    QAction *saveAct;
-    QAction *printAct;
-    QAction *exitAct;
-    QAction *undoAct;
-    QAction *redoAct;
-    QAction *cutAct;
-    QAction *copyAct;
-    QAction *pasteAct;
-    QAction *boldAct;
-    QAction *italicAct;
-    QAction *leftAlignAct;
-    QAction *rightAlignAct;
-    QAction *justifyAct;
-    QAction *centerAct;
-    QAction *setLineSpacingAct;
-    QAction *setParagraphSpacingAct;
-    QAction *aboutAct;
-    QAction *aboutQtAct;
-    QLabel *infoLabel;
+        QDockWidget *sidePanel1;
+        QDockWidget *sidePanel2;
+
+        //// Top Menus
+        QMenu *fileMenu;
+        QMenu *editMenu;
+        QMenu *searchMenu;
+        QMenu *workSpaceMenu;
+        QMenu *optionsMenu;
+        QMenu *toolsMenu;
+        QMenu *viewsMenu;
+        QMenu *windowMenu;
+        QMenu *helpMenu;
+        QMenu *customMenu;
+
+        QMenu* viewMenu;
+        QTextEdit *textEditor;
+
 };
-//! [3]
 
 
 #endif // MAINWINDOW_H
